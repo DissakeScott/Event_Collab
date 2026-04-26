cd3a3a7dad2d9124d856d899c4273ea83869dd51
yvesalexandrescott.dissakedissake <yvesalexandrescott.dissakedissake@et.esiea.fr>
2026-04-26 04:54:02 +0200

    chore: suppression ticket-service pour reorganisation commits

--- ticket-service/src/main/java/com/eventcollab/ticket/domain/TicketStatus.java
+++
@@ -1,7 +0,0 @@
-package com.eventcollab.ticket.domain;
-
-public enum TicketStatus {
-    ACTIVE,     // billet valide
-    CANCELLED,  // annulé par l utilisateur
-    USED        // scanné au check-in
-}
